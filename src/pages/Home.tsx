d453311449d211ff1dba69c30008cbf6bdfbaa95
juancarreonde <juancarreon@gmail.com>
2024-09-01 15:33:59 -0600

    add react icons, footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
-import { Button } from "../components/common/Button"
-import { Nav } from "../components/common/Nav"
+import { Button } from "../components/common/Button";
+import { Footer } from "../components/common/Footer";
+import { Nav } from "../components/common/Nav";
 
 export const Home = () => {
   return (
-    <div className="">
-      <Nav/>
+    <>
+      <Nav />
       <h1 className="gold w-fit">Hii</h1>
-      <Button/>
-    </div>
-  )
-}
+      <Button />
+      <Footer />
+    </>
+  );
+};
